8d35bbc3d83c806871ebb98d279bc911c1e6d6db
eullersa <eullersantos118@gmail.com>
2025-04-28 12:51:25 -0300

    feat: insert title and description on layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,15 @@ import StyledComponentsRegistry from "@/lib/styled-components-registry";
 import { StyledComponentProvider } from "@/providers/StyledComponentProvider";
 import { GlobalStyle } from "@/styles/GlobalStyles";
 import { InitializeMode } from "@/providers/InitializeMode";
+import { Metadata } from "next";
 
 type RootLayoutProps = {
   children: React.ReactNode;
+};
+
+export const metadata: Metadata = {
+  title: "Calendar",
+  description: "A simple calendar app",
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
